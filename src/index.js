be1eecf7a36a8282222e7260e474dd1cde13355f
Nikola Okuka <nikolaokuka@gmail.com>
2022-08-06 18:18:02 +0200

    add redux persist for theme persisting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import App from './App';
 
-import store from './store/store';
+import {store, persistor} from './store/store';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
